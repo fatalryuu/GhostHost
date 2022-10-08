be6f27894f1ad2b65ab61b92958a694a864ff15e
DrEiZ3R <92427151+fatalryuu@users.noreply.github.com>
2022-10-08 17:37:01 +0300

    Add files via upload

---
+++ script/slider.js
@@ -0,0 +1,31 @@
+const delay = 500;
+const current_slide = 1;
+const animation_del = 3000;
+
+let slider = document.querySelector('.slider'); //получение объекта слайдера
+slider.addEventListener('click', (event) => { //событие клика на элемент
+    if (!event.target.classList.contains('icon')) return; //определение клика по иконке
+    
+    let id = event.target.getAttribute('data-id'); // если клик по иконке, забираем атрибут
+    showSliderId(id);
+});
+
+initSlider();
+
+
+function showSliderId(id) {
+    let current = slider.querySelector('[data-slide="'+ id + '"]'); //находим текущий слайд
+    
+    //скрываем другие слайды
+    slider.querySelectorAll(".slide").forEach((slide) => { //массив из всех слайдов
+        slide.style.opacity = 0; //скрываем
+        setTimeout(() => { //скрываем визуально, не из дерева
+            slide.classList.add('hidden');
+        }, delay);
+    });
+    
+    setTimeout(() => { //показываем текущий слайд
+        current.classList.remove('hidden');
+        current.style.opacity = 1; //показываем слайд
+    }, delay)
+}
